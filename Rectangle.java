4b20167f80a546278a2a1e0c0fb13b00155cdc68
odwanodada <odwanodada22@gmail.com>
2021-05-06 09:17:56 +0200

    RectangleCal

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+      double width = 2.5;
+      double height = 8.1;
+      double area = width * height;
+          
+      System.out.println("The area of the rectangle is:");
+      System.out.println(area);
+    }
+  }
